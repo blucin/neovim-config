b7aeb608973d6388dffd20e2f19530f1201eed93
Akshar Patel <glaivesharp@gmail.com>
2024-04-08 17:11:10 +0530

    Add snippets and lsp autocompletion

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -23,12 +23,13 @@ return {
     -- list: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
     "neovim/nvim-lspconfig",
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default -- for snippets
       local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.eslint.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.typst_lsp.setup({})
+      lspconfig.lua_ls.setup({ capabilities = capabilities })
+      lspconfig.tsserver.setup({ capabilities = capabilities })
+      lspconfig.eslint.setup({ capabilities = capabilities })
+      lspconfig.gopls.setup({ capabilities = capabilities })
+      lspconfig.typst_lsp.setup({ capabilities = capabilities })
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})

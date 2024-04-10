7cdff6a393c23269314455c1ea0f18d8e0decf36
Akshar Patel <glaivesharp@gmail.com>
2024-04-10 14:28:10 +0530

    Add buffer, path, cmdline completion and wakatime analytics

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,7 +1,8 @@
 return {
-  {
-		"hrsh7th/cmp-nvim-lsp",
-  },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/cmp-buffer" },
+	{ "hrsh7th/cmp-path" },
+	{ "hrsh7th/cmp-cmdline" },
 	{
 		"L3MON4D3/LuaSnip",
 		dependencies = {
@@ -40,6 +41,23 @@ return {
 				}, {
 					{ name = "buffer" },
 				}),
+			})
+			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline({ "/", "?" }, {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = {
+					{ name = "buffer" },
+				},
+			})
+			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+			cmp.setup.cmdline(":", {
+				mapping = cmp.mapping.preset.cmdline(),
+				sources = cmp.config.sources({
+					{ name = "path" },
+				}, {
+					{ name = "cmdline" },
+				}),
+				matching = { disallow_symbol_nonprefix_matching = false },
 			})
 		end,
 	},
